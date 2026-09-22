1e2a62761b8087ed832e7b9535b5e8acf00cbf55
Za <Zubuhle01@gmail.com>
2026-09-22 19:19:52 +0200

    MainMenu Added

--- Assets/Project/Scripts/UI/Main Menu Manager.cs
+++ Assets/Project/Scripts/UI/Main Menu Manager.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject mainPanel;
+    public GameObject settingsPanel;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  public void PlayGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenSettings()
     {
-        
+        mainPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void Back()
+    {
+        settingsPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
